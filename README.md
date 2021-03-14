8a9832735d80c90a26adbdf625e0ce4244aae4ed
Pavel <popovpp@yandex.ru>
2021-03-14 18:26:27 +0300

    Ended code after testing

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # disk_info
-This script returns information about disk device in Linux by analysis of path string in an input file.
+This script returns information about disk device in Linux by analysis of path string in an inputfile.
+The script uses utilities 'lsblk' and 'df'.
+Format of inputfile must be likes this:
+/dev/sda
+Output information format folow:
+path, device type, size GB, avalible MB, file system type, mountpoint
+For example:
+/dev/sda1 part 0.512G 511M vfat /boot/efi
+To run the script use: 
+python dinfo.py [inputfile name]
--- dinfo.py
+++ dinfo.py
@@ -5,18 +5,32 @@ from sys import argv
 
 if __name__ == '__main__':
     
-    input_filename = argv[1]
-    list_files = subprocess.run("lsblk > lsblk.txt", shell=True)
-    list_files1 = subprocess.run("df -h --output=source,fstype,size,avail,target -x tmpfs -x devtmpfs > df.txt", 
-                                     shell=True)
     try:
+        list_files = subprocess.run("lsblk > lsblk.txt", shell=True, check=True)
+    except Exception as e:
+        os.remove('lsblk.txt')
+        exit(0)
+    
+    try:
+        list_files1 = subprocess.run(
+                         "df -h --output=source,fstype,size,avail,target -x tmpfs -x devtmpfs > df.txt", 
+                         shell=True, check=True)
+    except Exception as e:
+        os.remove('lsblk.txt')
+        os.remove('df.txt')
+        exit(0)
+    
+    try:
+        input_filename = argv[1]
         with open(input_filename, 'r') as f:
             dev_path = f.readline().strip()
     except:
-        print('Use: python dimfo.py <input filename>')
+        print('Use: python dinfo.py <inputfile name>')
         exit(0)
+    
     with open('lsblk.txt', 'r') as f:
-        s_title = f.readline()
+        flag1 = False
+        f.readline()
         for s in f:
             while not s[0].isalpha():
                 s = s[1:]
@@ -24,46 +38,68 @@ if __name__ == '__main__':
             if s.rsplit()[0].strip() == dev_path:
                 size = s.rsplit()[3].replace('\n','')
                 if size[-1].lower() == 'k':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))/1000000) + 'G'
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))/1000000) + 'G'
                 elif size[-1].lower() == 'm':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))/1000) + 'G'
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))/1000) + 'G'
                 elif size[-1].lower() == 't':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))*1000) + 'G'
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))*1000) + 'G'
                 elif size[-1].lower() == 'p':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))*1000000) + 'G'
+                    size = str(float(size[0:len(size)-1].
+                    	                  eplace(',', '.'))*1000000) + 'G'
                 elif size[-1].lower() == 'e':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))*1000000000) + 'G'
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))*1000000000) + 'G'
                 elif size[-1].lower() == 'z':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))*1000000000000) + 'G'
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))*1000000000000) + 'G'
                 elif size[-1].lower() == 'y':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))*1000000000000000) + 'G'
-                s_print = ' '.join((dev_path, s.rsplit()[5].replace('\n',''), size))
-#                    print(s_print)
-                os.remove('lsblk.txt')
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))*1000000000000000) + 'G'
+                s_print = ' '.join((dev_path, s.rsplit()[5].replace('\n',''), size))               
+                flag1 = True
                 break
+    
     with open('df.txt', 'r') as f:
-        s_title = f.readline()
+        flag2 = False
+        f.readline()
         for s in f:
-            print(s.rsplit()[0].strip())
             if s.rsplit()[0].strip() == dev_path:
-                av_size = s.rsplit()[3].replace('\n','')
+                size = s.rsplit()[3].replace('\n','')
                 if size[-1].lower() == 'k':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))/1000) + 'M'
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))/1000) + 'M'
                 elif size[-1].lower() == 'g':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))*1000) + 'M'
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))*1000) + 'M'
                 elif size[-1].lower() == 't':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))*1000000) + 'M'
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))*1000000) + 'M'
                 elif size[-1].lower() == 'p':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))*1000000000) + 'M'
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))*1000000000) + 'M'
                 elif size[-1].lower() == 'e':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))*1000000000000) + 'M'
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))*1000000000000) + 'M'
                 elif size[-1].lower() == 'z':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))*1000000000000000) + 'M'
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))*1000000000000000) + 'M'
                 elif size[-1].lower() == 'y':
-                    size = str(float(size[0:len(size)-1].replace(',', '.'))*1000000000000000000) + 'M'
+                    size = str(float(size[0:len(size)-1].
+                    	                  replace(',', '.'))*1000000000000000000) + 'M'
                 s_print1 = ' '.join((size, s.rsplit()[1].replace('\n',''), 
-                                     s.rsplit()[4].replace('\n','')))
-                print(s_print, s_print1)
-                os.remove('df.txt')
+                                     s.rsplit()[4].replace('\n','')))                
+                flag2 = True
                 break
+    
+    os.remove('lsblk.txt')
+    os.remove('df.txt')
 
+    if flag1&(~flag2):
+        print(s_print)
+    elif flag1&flag2:
+        print(s_print, s_print1)
+    else:
+    	print('The path in the inputfile is not exist.')
